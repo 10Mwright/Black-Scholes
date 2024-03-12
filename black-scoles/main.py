bbfacc444288cadb055d6af3116aa25b2a3ff72a
10Mwright <Matthew.wright.2018@live.rhul.ac.uk>
2024-03-12 21:07:10 +0000

    implemented initial version of black-scholes equation

--- black-scoles/main.py
+++ black-scoles/main.py
@@ -1,5 +1,5 @@
 import sys
-import yfinance as yf
+import numpy as np
 
 import data
 
@@ -9,5 +9,20 @@ def main():
     ticker = data.getTicker(tickerSymbol)
 
     print(ticker)
+
+    calculate(54, 53, 0.2, 5, 0.2) ## TEMP ##
+
+def calculate(currentPrice, strikePrice, rfInterestRate, time, volatility):
+    
+    d1 = (np.emath.logn(strikePrice, currentPrice) + (rfInterestRate + ((np.square([volatility]) / 2) * time))) / volatility * np.sqrt(time)
+    
+    d2 = d1 - volatility * np.sqrt(time)
+
+    nDistributionD1 = np.random.normal(d1, 1, 1) # TODO: Review ##
+    nDistributionD2 = np.random.normal(d2, 1, 1) # TODO: Review ##
+
+    optionPrice = (currentPrice * nDistributionD1 * d1) - strikePrice * np.exp(-rfInterestRate*time) * nDistributionD2 * d2
+
+    print("Black-Scholes value is: ", optionPrice)
 
 main()
